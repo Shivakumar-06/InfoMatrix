f737799b0456fcc04f600f022bc8b736a42a888d
Shivakumar-06 <shivakumar@nirvaaha.com>
2026-01-02 17:10:02 +0530

    added

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://infomatrix.netlify.app/"
+      "https://infomatrix.netlify.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
