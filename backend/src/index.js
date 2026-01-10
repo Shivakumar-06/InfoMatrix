4ba1f4062a6b19f6668baac673fb5b6c736bf8de
Shivakumar-06 <shivakumar@nirvaaha.com>
2026-01-10 12:24:19 +0530

    add cors domain

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://infomatrix.netlify.app"
+      "https://infomatrix.vercel.app/login"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
